fe03ed42044f068c1379196364269ba1d35d1bd6
TrihstonMadden <31551067+TrihstonMadden@users.noreply.github.com>
2018-11-16 12:18:09 -0800

    Update nato-tuple.py

--- nato-tuple.py
+++ nato-tuple.py
@@ -3,7 +3,7 @@
 nato = (["A", "ALPHA"], ["B", "BRAVO"], ["C", "CHARLIE"],
 		["D", "DELTA"], ["E", "ECHO"], ["F", "FOXTROT"],
 		["G", "GOLF"], ["H", "HOTEL"], ["I", "INDIA"],
-		["J", "JULIET"], ["K", "KILO"]], ["L", "LIMA"],
+		["J", "JULIET"], ["K", "KILO"], ["L", "LIMA"],
 		["M", "MIKE"], ["N", "NOVEMBER"], ["O", "OSCAR"],
 		["P", "PAPA"], ["Q", "QUEBEC"], ["R", "ROMEO"],
 		["S", "SIERRA"], ["T", "TANGO"], ["U", "UNIFORM"],
