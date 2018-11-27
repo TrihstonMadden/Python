a79e8fa2d2701722067dfae63838c1a671cf5e68
TrihstonMadden <31551067+TrihstonMadden@users.noreply.github.com>
2018-11-27 13:32:29 -0800

    Add files via upload

---
+++ sets_1.py
@@ -0,0 +1,16 @@
+#set_1.py tlm
+alpha = {"a","bb","ccc","dddd","eeeee","ffffff"}
+for x in alpha:
+	#print(alpha[x]) 
+	print(x)
+print(sorted(alpha))
+
+"""
+
+Look at what is going on here.
+a set can not be indexed.
+for x in alpha looks at all the elements in
+the set and assigns one at a time to the x
+variable.
+the following is an excellent resource.
+"""
---
+++ sets_3.py
@@ -0,0 +1,12 @@
+#set_3.py tlm
+alpha = {"a","b","c","d","e","f"}
+bravo = {"z","y","x","w","v"}
+charlie = alpha.union(bravo)
+for x in charlie:
+	print(x," ",end="c")
+print("pop 1")
+thepop = charlie.pop()
+print(thepop)
+print("pop 2")
+thepop = charlie.pop()
+print (thepop)
