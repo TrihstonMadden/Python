78ee52bd2a295bcc9ab7ba79821512adfb23876c
TrihstonMadden <31551067+TrihstonMadden@users.noreply.github.com>
2019-01-22 12:33:25 -0800

    Add files via upload

---
+++ Lambda3.py
@@ -0,0 +1,16 @@
+#file 3 : quadratic_function
+def quadratic_function(a,b,c):
+	return lambda x: a*(x**2) + b* x + c
+	
+a1 = quadratic_function(1,2,1)
+print(a1(1))
+print("Input a,b and c from a quadratic_function in the form of ")
+print("x = ax^2 + bx + c")
+# we have a problem here
+a = input("Input a ")
+b = input("Input b ")
+c = input("Input c ")
+a2 = quadratic_function(a,b,c)
+fx = a2(1)
+print("f(x) = ",fx)
+# end of the quadratic.py files
