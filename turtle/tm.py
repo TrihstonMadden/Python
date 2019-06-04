32d09f2d1d183e4ad16eec6c523a67cbc5e6bff8
TrihstonMadden <31551067+TrihstonMadden@users.noreply.github.com>
2019-06-04 11:56:29 -0700

    Add files via upload

---
+++ turtle/tm.py
@@ -0,0 +1,32 @@
+from turtle import *
+def trihston():
+    speed(0)
+    pencolor('red')
+    bgcolor('black')
+    x = 0
+    up()
+
+    rt(45)
+    fd(90)
+    rt(135)
+
+    down()
+    while x < 100:
+
+        fd(200)
+        rt(61)
+        fd(200)
+        rt(61)
+        fd(200)
+        rt(61)
+        fd(200)
+        rt(61)
+        fd(200)
+        rt(61)
+        fd(200)
+        rt(61)
+
+        rt(12.1111)
+        x = x+1
+
+        #exitonclick()
---
+++ turtle/turtleg.py
@@ -0,0 +1,33 @@
+from turtle import *
+
+
+speed(0)
+pencolor('red')
+bgcolor('black')
+
+x = 0
+up()
+rt(45)
+fd(90)
+rt(135)
+down()
+while x < 90:
+
+    fd(200)
+    rt(61)
+    fd(200)
+    rt(61)
+    fd(200)
+    rt(61)
+    fd(200)
+    rt(61)
+    fd(200)
+    rt(61)
+    fd(200)
+    rt(61)
+
+    rt(33.1111)
+    x = x+1
+
+
+exitonclick()
